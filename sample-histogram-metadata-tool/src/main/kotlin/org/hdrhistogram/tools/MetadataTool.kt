936c83cd38aeed33ce27904b22b82968dbec5fcd
Marshall Pierce <marshall@mpierce.org>
2017-09-27 10:20:31 -0500

    Use lazy traversal to avoid allocating big collections. Use a thread pool so files can be handled in parallel.

--- sample-histogram-metadata-tool/src/main/kotlin/org/hdrhistogram/tools/MetadataTool.kt
+++ sample-histogram-metadata-tool/src/main/kotlin/org/hdrhistogram/tools/MetadataTool.kt
@@ -2,79 +2,120 @@ package org.hdrhistogram.tools
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
+import joptsimple.OptionException
+import joptsimple.OptionParser
+import joptsimple.OptionSet
+import org.HdrHistogram.AbstractHistogram
 import org.HdrHistogram.Histogram
 import org.HdrHistogram.HistogramIterationValue
+import org.HdrHistogram.HistogramLogReader
 import org.tukaani.xz.LZMA2Options
 import org.tukaani.xz.XZOutputStream
 import java.io.File
 import java.nio.ByteBuffer
 import java.nio.file.Files
+import java.nio.file.Path
+import java.time.Duration
+import java.time.Instant
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
 
 object MetadataTool {
     @JvmStatic
     fun main(args: Array<String>) {
-        // provide directory containing histograms
-        val dir = File(args[0])
+        val parser = OptionParser()
+        val dirOpt = parser.accepts("dir", "Dir with histogram samples")
+                .withRequiredArg().ofType(String::class.java).describedAs("directory").required()
 
-        val writer = ObjectMapper().writerWithDefaultPrettyPrinter()
-
-        Files.newDirectoryStream(dir.toPath(), "*.histo").use { stream ->
-            stream.forEach { path ->
-                println("Processing $path")
-
-                val buf = ByteBuffer.wrap(Files.readAllBytes(path))
-                val histo = Histogram.decodeFromCompressedByteBuffer(buf, 1)
-
-                val metadata = HistogramMetadata(count = histo.totalCount,
-                        max = histo.maxValue,
-                        min = histo.minValue,
-                        lowestDiscernibleValue = histo.lowestDiscernibleValue,
-                        highestTrackableValue = histo.highestTrackableValue,
-                        significantValueDigits = histo.numberOfSignificantValueDigits,
-                        mean = histo.mean,
-                        stdDev = histo.stdDeviation,
-                        valuesAtPercentiles = (0..1000)
-                                // sample every 10th of a percent
-                                .map { it.toDouble() / 10 }
-                                // turn it into a map of percentile to value
-                                .associateBy({ it }, { i -> histo.getValueAtPercentile(i) }),
-                        iterators = Iterators(
-                                linear = (0..14)
-                                        // linear stride with strides at several powers of 2
-                                        .map { 1L.shl(it) }
-                                        .associateBy({ "$it" },
-                                                { histo.linearBucketValues(it).map { IteratorValue(it) } }),
-                                percentile = (0..9)
-                                        // percentile iteration at several units-per-tick
-                                        .map { 1.shl(it) }
-                                        .associateBy({ "$it" }, { histo.percentiles(it).map { IteratorValue(it) } }),
-                                logarithmic = listOf(1L, 100, 1000)
-                                        // combine different first bucket and log bases into "firstbucket-logbase" names
-                                        .flatMap { firstBucketSize ->
-                                            listOf(1.1, 2.0, 10.0).map { logBase ->
-                                                Pair(firstBucketSize, logBase)
-                                            }
-                                        }
-                                        .associateBy({ "${it.first}-${it.second}" }, {
-                                            histo.logarithmicBucketValues(it.first, it.second).map { IteratorValue(it) }
-                                        }),
-                                recorded = histo.recordedValues().map { IteratorValue(it) },
-                                all = histo.allValues().map { IteratorValue(it) })
-                )
-
-                val metadataFileName = path.fileName.toString().replace(Regex("\\.histo$"), "-metadata.json.xz")
-                val metadataPath = path.parent.resolve(metadataFileName)
-                Files.newOutputStream(metadataPath).use { fos ->
-                    XZOutputStream(fos, LZMA2Options()).use { xzos ->
-                        writer.writeValue(xzos, metadata)
-                    }
-                }
-            }
+        var opts: OptionSet? = null
+        try {
+            opts = parser.parse(*args)
+        } catch (e: OptionException) {
+            parser.printHelpOn(System.err)
+            System.exit(1)
         }
+
+        val start = Instant.now()
+
+        val dir = File(opts!!.valueOf(dirOpt))
+
+        val jsonWriter = ObjectMapper().writerWithDefaultPrettyPrinter()
+
+        // It's trivial to parallelize
+        val ioPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
+
+        val futures = Files.newDirectoryStream(dir.toPath(), "*.histo").use { stream ->
+            stream.map { path ->
+                ioPool.submit(Callable<Path> {
+                    println("Starting $path")
+                    val buf = ByteBuffer.wrap(Files.readAllBytes(path))
+                    val histo = Histogram.decodeFromCompressedByteBuffer(buf, 1)
+
+                    val testDataPath = replaceSuffix(path, Regex("\\.histo$"), "-metadata.json.xz")
+                    Files.newOutputStream(testDataPath).use { fos ->
+                        XZOutputStream(fos, LZMA2Options()).use { xzos ->
+                            jsonWriter.writeValue(xzos, HistogramTestData(histo))
+                        }
+                    }
+
+                    path
+                })
+            }.toMutableList()
+        }
+
+        futures.addAll(Files.newDirectoryStream(dir.toPath(), "*.hlog").use { stream ->
+            stream.map { path ->
+                ioPool.submit(Callable<Path> {
+                    println("Starting $path")
+                    val reader = HistogramLogReader(path.toFile())
+
+                    val histograms = mutableListOf<HistogramLogEntry>()
+
+                    while (reader.hasNext()) {
+                        val h = reader.nextIntervalHistogram() as AbstractHistogram
+
+                        histograms.add(HistogramLogEntry(h))
+                    }
+
+                    val testDataPath = replaceSuffix(path, Regex("\\.hlog$"), "-metadata.json.xz")
+                    Files.newOutputStream(testDataPath).use { fos ->
+                        XZOutputStream(fos, LZMA2Options()).use { xzos ->
+                            jsonWriter.writeValue(xzos, HistogramLogTestData(histograms))
+
+                        }
+                    }
+
+                    path
+                })
+            }.toMutableList()
+        })
+
+        futures.forEach { f ->
+            println("${f.get()} finished")
+        }
+
+        ioPool.shutdown()
+
+        println("Elapsed time: ${Duration.between(start, Instant.now())}")
     }
 }
 
-class HistogramMetadata(@JsonProperty("totalCount") val count: Long,
+private fun replaceSuffix(path: Path, suffixPattern: Regex, replacement: String): Path {
+    val metadataFileName = path.fileName.toString().replace(suffixPattern, replacement)
+    return path.parent.resolve(metadataFileName)
+}
+
+
+class HistogramLogEntry(@JsonProperty("tag") val tag: String?,
+                        @JsonProperty("startTime") val startTime: Long,
+                        @JsonProperty("endTime") val endTime: Long,
+                        @JsonProperty("histogram") val histogram: HistogramTestData) {
+    constructor(h: AbstractHistogram) : this(h.tag, h.startTimeStamp, h.endTimeStamp, HistogramTestData(h))
+}
+
+class HistogramLogTestData(@JsonProperty("entries") val entries: List<HistogramLogEntry>)
+
+class HistogramTestData(@JsonProperty("totalCount") val count: Long,
                         @JsonProperty("maxValue") val max: Long,
                         @JsonProperty("minValue") val min: Long,
                         @JsonProperty("lowestDiscernibleValue") val lowestDiscernibleValue: Long,
@@ -83,8 +124,67 @@ class HistogramMetadata(@JsonProperty("totalCount") val count: Long,
                         @JsonProperty("mean") val mean: Double,
                         @JsonProperty("stdDev") val stdDev: Double,
                         @JsonProperty("valuesAtPercentiles") val valuesAtPercentiles: Map<Double, Long>,
-                        @JsonProperty("iterators") val iterators: Iterators
-)
+                        @JsonProperty("iterators") val iterators: Iterators) {
+    constructor(h: AbstractHistogram) : this(count = h.totalCount,
+            max = h.maxValue,
+            min = h.minValue,
+            lowestDiscernibleValue = h.lowestDiscernibleValue,
+            highestTrackableValue = h.highestTrackableValue,
+            significantValueDigits = h.numberOfSignificantValueDigits,
+            mean = h.mean,
+            stdDev = h.stdDeviation,
+            valuesAtPercentiles = (0..1000)
+                    // sample every 10th of a percent
+                    .map { it.toDouble() / 10 }
+                    // turn it into a map of percentile to value
+                    .associateBy({ it }, { i -> h.getValueAtPercentile(i) }),
+            iterators = Iterators(
+                    linear = (0..14)
+                            // linear stride with strides at several powers of 2
+                            .map { 1L.shl(it) }
+                            .associateBy({ "$it" },
+                                    // Use a Sequence to keep the transformation lazy
+                                    {
+                                        h.linearBucketValues(it)
+                                                .asSequence()
+                                                .map { IteratorValue(it) }
+                                                .asIterable()
+                                    }),
+                    percentile = (0..9)
+                            // percentile iteration at several units-per-tick
+                            .map { 1.shl(it) }
+                            .associateBy({ "$it" },
+                                    {
+                                        h.percentiles(it)
+                                                .asSequence()
+                                                .map { IteratorValue(it) }
+                                                .asIterable()
+                                    }),
+                    logarithmic = listOf(1L, 100, 1000)
+                            // There are two ways to vary a logarithmic iteration: the first bucket size, and the log
+                            // base. So, we map pairs of first bucket and log bases into "firstbucket-logbase" names to
+                            // use in the JSON.
+                            .flatMap { firstBucketSize ->
+                                listOf(1.1, 2.0, 10.0).map { logBase ->
+                                    Pair(firstBucketSize, logBase)
+                                }
+                            }
+                            .associateBy({ "${it.first}-${it.second}" }, {
+                                h.logarithmicBucketValues(it.first, it.second)
+                                        .asSequence()
+                                        .map { IteratorValue(it) }
+                                        .asIterable()
+                            }),
+                    recorded = h.recordedValues()
+                            .asSequence()
+                            .map { IteratorValue(it) }
+                            .asIterable(),
+                    all = h.allValues()
+                            .asSequence()
+                            .map { IteratorValue(it) }
+                            .asIterable())
+    )
+}
 
 class IteratorValue(@JsonProperty("valueIteratedTo") val valueIteratedTo: Long,
                     @JsonProperty("countAddedInThisIterationStep") val countAddedInThisIterationStep: Long,
@@ -101,9 +201,9 @@ class IteratorValue(@JsonProperty("valueIteratedTo") val valueIteratedTo: Long,
     )
 }
 
-class Iterators(@JsonProperty("linear") val linear: Map<String, List<IteratorValue>>,
-                @JsonProperty("percentile") val percentile: Map<String, List<IteratorValue>>,
-                @JsonProperty("logarithmic") val logarithmic: Map<String, List<IteratorValue>>,
-                @JsonProperty("recorded") val recorded: List<IteratorValue>,
-                @JsonProperty("all") val all: List<IteratorValue>
+class Iterators(@JsonProperty("linear") val linear: Map<String, Iterable<IteratorValue>>,
+                @JsonProperty("percentile") val percentile: Map<String, Iterable<IteratorValue>>,
+                @JsonProperty("logarithmic") val logarithmic: Map<String, Iterable<IteratorValue>>,
+                @JsonProperty("recorded") val recorded: Iterable<IteratorValue>,
+                @JsonProperty("all") val all: Iterable<IteratorValue>
 )
